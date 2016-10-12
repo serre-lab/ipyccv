63d26d03f1796cc8900148b91d2b0bf0fc64d328
Ben Navetta <ben.navetta@gmail.com>
2016-10-12 10:54:11 -0400

    add button for tutorial 5

--- forward_server.py
+++ forward_server.py
@@ -49,7 +49,8 @@ class ClientData:
             'Tutorial 1': '/notebooks/Tutorial_1.ipynb',
             'Tutorial 2': '/notebooks/Tutorial_2.ipynb',
             'Tutorial 3': '/notebooks/Tutorial_3.ipynb',
-            'Tutorial 4': '/notebooks/Tutorial_4.ipynb'
+            'Tutorial 4': '/notebooks/Tutorial_4.ipynb',
+            'Tutorial 5': '/notebooks/Tutorial_5.ipynb'
         }
     }
 
